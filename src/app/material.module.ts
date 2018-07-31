5caabc88e0568b792f2f1550a1f013fc23df65cc
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-07-31 20:21:17 +0530

    implemented date picker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field'
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
 @NgModule({
 
     imports: [
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatNativeDateModule
     ],
     exports: [
         MatButtonModule,
         MatIconModule,
         MatInputModule,
-        MatFormFieldModule
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatNativeDateModule
     ]
 
 })
