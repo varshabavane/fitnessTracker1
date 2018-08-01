324557060bf946eea46c17dda46e33b5a826217a
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-01 17:22:11 +0530

    integrated tabs component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,6 +9,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
 @NgModule({
 
     imports: [
@@ -22,6 +23,7 @@ import { MatListModule } from '@angular/material/list';
         MatSidenavModule,
         MatToolbarModule,
         MatListModule,
+        MatTabsModule
 
     ],
     exports: [
@@ -34,7 +36,8 @@ import { MatListModule } from '@angular/material/list';
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule
+        MatListModule,
+        MatTabsModule
     ]
 
 })
