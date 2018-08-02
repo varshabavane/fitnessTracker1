075be319044eb1c122fadd9d47ba38fad28d0731
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-02 16:46:05 +0530

    dialog implemented with start and stop functionality

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { StopTrainingComponent } from './stop-training.component';
 @Component({
@@ -7,11 +7,17 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+
+  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: number;
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.startOrResumtTimer();
+  }
+
+  startOrResumtTimer() {
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       if (this.progress >= 100) {
@@ -19,13 +25,17 @@ export class CurrentTrainingComponent implements OnInit {
       }
     }, 1000);
   }
-
   onStop() {
     clearInterval(this.timer);
     const dialogRef = this.dialog.open(StopTrainingComponent, { data: { progress: this.progress } });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if (result) {
+        this.trainingExit.emit();
+      }else{
+        this.startOrResumtTimer()
+      }
+
     })
   }
 
