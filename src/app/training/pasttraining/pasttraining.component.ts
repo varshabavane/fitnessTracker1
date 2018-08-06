c73839148791e58a72506303806f5a5b455358ad
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-06 22:47:04 +0530

    table created on past training

--- src/app/training/pasttraining/pasttraining.component.ts
+++ src/app/training/pasttraining/pasttraining.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material";
+import { Exercise } from "../exercise.model";
 
 @Component({
-  selector: 'app-pasttraining',
-  templateUrl: './pasttraining.component.html',
-  styleUrls: ['./pasttraining.component.css']
+  selector: "app-pasttraining",
+  templateUrl: "./pasttraining.component.html",
+  styleUrls: ["./pasttraining.component.css"]
 })
 export class PasttrainingComponent implements OnInit {
+  displayedColumns = ["date", "name", "duration", "calories", "state"];
+  dataSource = new MatTableDataSource<Exercise>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
