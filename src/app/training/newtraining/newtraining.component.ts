26682bf24031f675e045891bcb9fd9a98a151e12
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-08 19:58:44 +0530

    tested firebase connection

--- src/app/training/newtraining/newtraining.component.ts
+++ src/app/training/newtraining/newtraining.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { TrainingService } from "../training.service";
 import { Exercise } from "../exercise.model";
 import { NgForm } from "@angular/forms";
-
+import { AngularFirestore } from "angularfire2/firestore";
 @Component({
   selector: "app-newtraining",
   templateUrl: "./newtraining.component.html",
@@ -10,10 +10,19 @@ import { NgForm } from "@angular/forms";
 })
 export class NewtrainingComponent implements OnInit {
   private exercises: Exercise[] = [];
-  constructor(private trainingExercise: TrainingService) {}
+  constructor(
+    private trainingExercise: TrainingService,
+    private db: AngularFirestore
+  ) {}
 
   ngOnInit() {
-    this.exercises = this.trainingExercise.getAvailableExercises();
+    // this.exercises = this.trainingExercise.getAvailableExercises();
+    this.db
+      .collection("AvailableExercises")
+      .valueChanges()
+      .subscribe(result => {
+        console.log(result);
+      });
   }
 
   onStartTraining(form: NgForm) {
