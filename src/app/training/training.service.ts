198c4924c50724126aeef9d76e560458742d6ddb
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-09 22:21:09 +0530

    created a test method to add data to database

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -46,7 +46,7 @@ export class TrainingService {
     this.exerciseChanged.next({ ...this.runningExercise });
   }
   completeExercise() {
-    this.exercises.push({
+    this.addDataToDatabase({
       ...this.runningExercise,
       date: new Date(),
       state: "completed"
@@ -55,7 +55,7 @@ export class TrainingService {
     this.exerciseChanged.next(null);
   }
   cancelExercise(progress: number) {
-    this.exercises.push({
+    this.addDataToDatabase({
       ...this.runningExercise,
       duration: this.runningExercise.duration * (progress / 100),
       calories: this.runningExercise.calories * (progress / 100),
@@ -71,5 +71,9 @@ export class TrainingService {
 
   getCompletedOrCancelledExercises() {
     return this.exercises.slice();
+  }
+
+  private addDataToDatabase(exercise: Exercise) {
+    this.db.collection("finishedExercise").add(exercise);
   }
 }
