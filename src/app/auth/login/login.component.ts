fcbff625d3f23bc6534bb06d7c0dc4f98ff160f8
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-12 14:46:59 +0530

    spinner implemented on login and signup page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
-import { AuthService } from '../auth.service';
-
+import { AuthService } from "../auth.service";
+import { UIService } from "../../shared/ui.service";
+import { Subscription } from "rxjs";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
+  isLoading = false;
+  private loadingSubs: Subscription;
 
-  constructor(private authService: AuthService) {}
-/* REactive Form approach */
+  constructor(private authService: AuthService, private uiService: UIService) {}
+  /* REactive Form approach */
   ngOnInit() {
+    this.loadingSubs = this.uiService.loadingStateChanged.subscribe(
+      isLoading => {
+        this.isLoading = isLoading;
+      }
+    );
     this.loginForm = new FormGroup({
-      email: new FormControl('', {
+      email: new FormControl("", {
         validators: [Validators.required, Validators.email]
       }),
-      password: new FormControl('', { validators: [Validators.required] })
+      password: new FormControl("", { validators: [Validators.required] })
     });
   }
 
@@ -27,5 +35,9 @@ export class LoginComponent implements OnInit {
       email: this.loginForm.value.email,
       password: this.loginForm.value.password
     });
+  }
+
+  ngOnDestroy(){
+    this.loadingSubs.unsubscribe()
   }
 }
