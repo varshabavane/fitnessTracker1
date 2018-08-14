e37f57a9d485cd85ff117b4bc9532396fd912984
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-14 21:50:18 +0530

    module created for optimization

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -3,13 +3,15 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  Router
+  Router,
+  Route,
+  CanLoad
 } from "@angular/router";
 
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
@@ -19,5 +21,13 @@ export class AuthGuard implements CanActivate {
       this.router.navigate(["/login"]);
     }
     // return true; /* for debugging and testing purpose */
+  }
+
+  canLoad(route: Route) {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(["/login"]);
+    }
   }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { SignupComponent } from "./signup/signup.component";
 import { LoginComponent } from "./login/login.component";
-
-import { CommonModule } from "@angular/common";
+import { SharedModule } from "../shared/shared.module";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { AuthRoutingModule } from "./auth-routing.module";
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    SharedModule,
+    AngularFireAuthModule,
+    AuthRoutingModule
+  ],
   exports: []
 })
 export class AuthModule {}
