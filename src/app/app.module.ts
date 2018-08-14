3b3165c48653b86f31e4ae4f220279271d4fa6a2
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-14 21:50:18 +0530

    module created for optimization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FlexLayoutModule } from "@angular/flex-layout";
@@ -27,7 +26,6 @@ import { AuthModule } from "./auth/auth.module";
 @NgModule({
   declarations: [
     AppComponent,
-    
     TrainingComponent,
     CurrentTrainingComponent,
     NewtrainingComponent,
@@ -43,9 +41,7 @@ import { AuthModule } from "./auth/auth.module";
     MaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
-    FormsModule,
     AuthModule,
-    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule
