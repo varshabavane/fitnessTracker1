4e09d30178d46d4611a722ac1112d51086ae8f97
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-14 22:00:05 +0530

    module seperated and compiled

---
+++ src/app/training/training.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { TrainingComponent } from "./training.component";
+import { CurrentTrainingComponent } from "./current-training/current-training.component";
+import { StopTrainingComponent } from "./current-training/stop-training.component";
+import { SharedModule } from "../shared/shared.module";
+import { TrainingRoutingModule } from "./training-routing.module";
+import { NewtrainingComponent } from "./newtraining/newtraining.component";
+import { PasttrainingComponent } from "./pasttraining/pasttraining.component";
+
+@NgModule({
+  declarations: [
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewtrainingComponent,
+    PasttrainingComponent,
+    StopTrainingComponent
+  ],
+  imports: [SharedModule, TrainingRoutingModule],
+  entryComponents: [StopTrainingComponent]
+})
+export class TrainingModule {}
