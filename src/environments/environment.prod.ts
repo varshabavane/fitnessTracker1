1e2b128e46892c71500990c33cc0318d1d17fa92
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-15 22:13:46 +0530

    bug fixed and file added to git ignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCNlKFMofQqbdmbmFkQK_Q3f2jBb-q5Fqw",
+    authDomain: "tutfitnesstracker.firebaseapp.com",
+    databaseURL: "https://tutfitnesstracker.firebaseio.com",
+    projectId: "tutfitnesstracker",
+    storageBucket: "tutfitnesstracker.appspot.com",
+    messagingSenderId: "939328746452"
+  }
 };
