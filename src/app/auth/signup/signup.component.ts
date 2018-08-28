4f92e6826588ce0b3eda213bd7042bf6cc19e17c
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-28 22:07:20 +0530

    binded date value, but still there is some error

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService } from "../auth.service";
 import { UIService } from "../../shared/ui.service";
@@ -13,26 +13,46 @@ import { DateAdapter } from "@angular/material";
   styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
-  maxDate;
-
+  maxDate: Date;
   isLoading$: Observable<boolean>;
+
   constructor(
     private authService: AuthService,
     private store: Store<fromRoot.State>,
-    private dateAdapter: DateAdapter<Date>) {
-      this.dateAdapter.setLocale("en-in");
-    }
-  
+    private dateAdapter: DateAdapter<Date>
+  ) {
+    this.dateAdapter.setLocale("en-IN");
+  }
 
   ngOnInit() {
     this.isLoading$ = this.store.select(fromRoot.getIsLoading);
     this.maxDate = new Date();
-    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+    this.maxDate = new Date(
+      this.maxDate.getFullYear() - 18,
+      this.maxDate.getMonth(),
+      this.maxDate.getDate()
+    );
+    console.log(this.maxDate);
   }
   onSubmit(form: NgForm) {
     this.authService.registeredUser({
       email: form.value.email,
       password: form.value.password
     });
+  }
+
+  addEvent(a, $event, f: NgForm, d) {
+    console.log(
+      JSON.stringify(a) +
+        " : " +
+        JSON.stringify($event.value) +
+        "\n" +
+        f.value.birthdate.valid +
+        "\n" +
+        d +
+        "\n" +
+        f.control.controls.birthdate.status + "\n" + 
+        JSON.stringify(f.control.controls.birthdate.errors)
+    );
   }
 }
